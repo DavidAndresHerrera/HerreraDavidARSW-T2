5eed34d455be48f8bd74a2b44530f5553a05447d
David Herrera <damoya2000@gmail.com>
2020-10-22 14:42:49 -0500

    mapa

--- src/main/resources/static/js/apiClient.js
+++ src/main/resources/static/js/apiClient.js
@@ -1,10 +1,11 @@
 const apiUrl = "http://localhost:8080/Ciudades/"
+const heroku = "https://herreradavid-arsw-t2.herokuapp.com/Ciudades"
 apiClient = (function () {
 
     return {
         getDatos: function (ciudad, callback) {
             jQuery.ajax({
-                url: apiUrl+ ciudad,
+                url: heroku+ ciudad,
                 success: function (result) {
                     callback(result);
                 },
